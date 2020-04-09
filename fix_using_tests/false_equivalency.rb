ab0d98b1c6642bb8905da11d43e6cfa60d3f53e9
Sean Dever <seand0611@gmail.com>
2020-04-09 03:28:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,8 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1 || num = 2
+  if num = 1 || 2
      "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num = 2
+     "HAM HAM HAM IN MY TUMMY"
   end
  # binding.pry
 end
